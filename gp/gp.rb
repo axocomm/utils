90e97b935da1b4a8798fd3f50778d332c269e2fa
axocomm <axocomm@gmail.com>
2018-03-14 19:47:26 -0400

    Rename config_dir to profile_dir and add example

--- gp/gp.rb
+++ gp/gp.rb
@@ -3,19 +3,18 @@
 require 'optparse'
 require 'yaml'
 
-# CONFIG_DIR = File.join(Dir.home, '.gp')
-CONFIG_DIR = "#{Dir.pwd}/gp"
+PROFILE_DIR = "#{Dir.home}/.gp"
 
 LIST_LINE_RE = /^([a-z_]+) = (.*)$/
 
-def profiles(config_dir)
-  Dir.glob("#{config_dir}/*.yaml").map { |f| File.basename f, '.yaml' }
+def profiles(profile_dir)
+  Dir.glob("#{profile_dir}/*.yaml").map { |f| File.basename f, '.yaml' }
 end
 
-def list_profiles(config_dir)
+def list_profiles(profile_dir)
   puts 'Available Profiles'
   puts '------------------'
-  profiles(config_dir).each { |f| puts "- #{f}" }
+  profiles(profile_dir).each { |f| puts "- #{f}" }
 end
 
 def current_config
@@ -30,8 +29,8 @@ def current_config
   end
 end
 
-def switch_config(config_dir, profile_name)
-  filename = "#{config_dir}/#{profile_name}.yaml"
+def switch_config(profile_dir, profile_name)
+  filename = "#{profile_dir}/#{profile_name}.yaml"
   profile = YAML.load_file filename
   cmd = profile.map { |(k, v)| "gcloud config set #{k} #{v}" }.join(' && ')
   system(cmd)
@@ -45,9 +44,9 @@ def parse_args
       options[:list] = true
     end
 
-    options[:config_dir] = CONFIG_DIR
-    opts.on('-c', '--config-dir DIR', 'Configuration directory') do |dir|
-      options[:config_dir] = dir
+    options[:profile_dir] = PROFILE_DIR
+    opts.on('-d', '--profile-dir DIR', 'Profile directory') do |dir|
+      options[:profile_dir] = dir
     end
   end
 
@@ -57,16 +56,16 @@ end
 
 if $PROGRAM_NAME == $0
   opts = parse_args
-  config_dir = opts[:config_dir]
+  profile_dir = opts[:profile_dir]
 
   if opts[:list]
-    list_profiles config_dir
+    list_profiles profile_dir
   elsif profile = ARGV.shift
-    unless profiles(config_dir).include?(profile)
+    unless profiles(profile_dir).include?(profile)
       raise "Invalid profile #{profile}"
     end
 
-    switch_config opts[:config_dir], profile
+    switch_config opts[:profile_dir], profile
   else
     puts YAML.dump(current_config)
   end
